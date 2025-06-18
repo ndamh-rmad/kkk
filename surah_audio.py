c7d84aad51c484217d07a24666a822703d3e5b28
1 <alshehriazo@gmail.com>
2025-06-18 19:58:23 +0300

    Update surah_audio.py

--- surah_audio.py
+++ surah_audio.py
@@ -1,14 +1,12 @@
-
-# روابط لتلاوة كاملة بصوت مشاري العفاسي (مباشرة وقابلة للتنزيل)
 surahs = [
-    {"name": "الفاتحة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/001.mp3"},
-    {"name": "البقرة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/002.mp3"},
-    {"name": "آل عمران", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/003.mp3"},
-    {"name": "النساء", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/004.mp3"},
-    {"name": "المائدة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/005.mp3"},
-    {"name": "الأنعام", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/006.mp3"},
-    {"name": "الأعراف", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/007.mp3"},
-    {"name": "الأنفال", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/008.mp3"},
-    {"name": "التوبة", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/009.mp3"},
-    {"name": "يونس", "audio": "https://www.everyayah.com/data/Mishary_Alafasy_64kbps/010.mp3"}
+    {"name": "الفاتحة", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/001.mp3"},
+    {"name": "البقرة", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/002.mp3"},
+    {"name": "آل عمران", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/003.mp3"},
+    {"name": "النساء", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/004.mp3"},
+    {"name": "المائدة", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/005.mp3"},
+    {"name": "الأنعام", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/006.mp3"},
+    {"name": "الأعراف", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/007.mp3"},
+    {"name": "الأنفال", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/008.mp3"},
+    {"name": "التوبة", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/009.mp3"},
+    {"name": "يونس", "audio": "https://www.everyayah.com/data/Alafasy_128kbps/010.mp3"},
 ]
